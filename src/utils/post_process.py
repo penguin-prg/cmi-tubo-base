21b5d4822111fc243aa3103d3035cc0d5e70857b
penguin-prg <ryotapro2119@gmail.com>
2023-11-04 13:33:15 +0000

    ,

--- src/utils/post_process.py
+++ src/utils/post_process.py
@@ -1,10 +1,10 @@
 import numpy as np
 import polars as pl
 from scipy.signal import find_peaks
-
+import pandas as pd
 
 def post_process_for_seg(
-    keys: list[str], preds: np.ndarray, score_th: float = 0.01, distance: int = 5000
+    keys: list[str], preds: np.ndarray, score_th: float = 0.01, distance: int = 5000, ret_oof_df: bool = False
 ) -> pl.DataFrame:
     """make submission dataframe for segmentation task
 
@@ -20,9 +20,19 @@ def post_process_for_seg(
     unique_series_ids = np.unique(series_ids)
 
     records = []
+    dfs = []
     for series_id in unique_series_ids:
         series_idx = np.where(series_ids == series_id)[0]
         this_series_preds = preds[series_idx].reshape(-1, 2)
+
+        if ret_oof_df:
+            oof_df = pd.DataFrame({
+                "series_id": series_id,
+                "step": np.arange(len(this_series_preds)),
+                "onset_oof": this_series_preds[:, 0],
+                "wakeup_oof": this_series_preds[:, 1],
+            })
+            dfs.append(oof_df)
 
         for i, event_name in enumerate(["onset", "wakeup"]):
             this_event_preds = this_series_preds[:, i]
@@ -52,4 +62,8 @@ def post_process_for_seg(
     sub_df = pl.DataFrame(records).sort(by=["series_id", "step"])
     row_ids = pl.Series(name="row_id", values=np.arange(len(sub_df)))
     sub_df = sub_df.with_columns(row_ids).select(["row_id", "series_id", "step", "event", "score"])
-    return sub_df
+    if ret_oof_df:
+        oof_df = pd.concat(dfs, axis=0).reset_index(drop=True)
+        return sub_df, oof_df
+    else:
+        return sub_df
